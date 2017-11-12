603ba450eed88362281a933d58dabafe169a88a4
Nataliya Gapochka <natasha.gapochka@gmail.com>
2017-11-12 12:58:57 -0800

    cleaned up code

--- colorGame.js
+++ colorGame.js
@@ -1,110 +1,95 @@
-let numSquares = [6, 3];
-let colors = generateColors(numSquares[0]);
+let numSquares = 6;
+let colors = [];
+let pickedColor;
 
 let squares = document.querySelectorAll('.square');
-
-let pickedColor = randomColor();
-
 let feedback = document.getElementById('feedback');
-
 let topDiv = document.querySelector('.top');
-
 let color = document.getElementById('color');
-
 let resetBtn = document.querySelector('#reset');
+let modeBtns = document.querySelectorAll('.mode');
 
-let hardBtn = document.querySelector('#hard');
+start();
 
-let easyBtn = document.querySelector('#easy');
+function start() {
+  selectedMode();
+  setUp();
+  reset();
+} // end of start function
 
-resetBtn.textContent = 'New Colors';
+//responsible for selecting mode of the Game
+function selectedMode() {
+  //modeBtns eventListeners
+  for (let i = 0; i < modeBtns.length; i++) {
+    modeBtns[i].addEventListener('click', function() {
+      //removing selected class from all buttons
+      modeBtns[0].classList.remove('selected');
+      modeBtns[1].classList.remove('selected');
 
-// Easy game mode for 3 colors
-easyBtn.addEventListener('click', function() {
-  //select easy button
-  easyBtn.classList.add('selected');
+      //adding selected class to clicked button
+      this.classList.add('selected');
 
-  // remove selection from hard button
-  hardBtn.classList.remove('selected');
+      //figure out how many squares to show
+      //depending on what button user clicked
+      this.textContent === 'Easy' ? (numSquares = 3) : (numSquares = 6);
+      reset();
+    }); // end of eventListener for modeBtns
+  } // end of for loop
+} //end of function selectedMode
 
-  //generate and change colors on page
-  colors = generateColors(numSquares[1]);
-
-  //pick a new random color from array
-  pickedColor = randomColor();
-  color.textContent = pickedColor;
-  console.log(colors, pickedColor);
+//responsible for setting up game in hard mode by default
+function setUp() {
   for (let i = 0; i < squares.length; i++) {
-    if (colors[i]) {
-      squares[i].style.backgroundColor = colors[i];
-    } else {
-      // hide 3 botton squares
-      squares[i].style.display = 'none';
-    }
-  }
-});
+    //add click listener
+    squares[i].addEventListener('click', function() {
+      //find color of clicked square
+      let clickedColor = this.style.backgroundColor;
+      //compare it to pickedColor
+      if (clickedColor === pickedColor) {
+        feedback.textContent = 'Correct!';
+        changeColors(clickedColor);
+        topDiv.style.backgroundColor = clickedColor;
+        resetBtn.textContent = 'Play again?';
+      } else {
+        this.style.backgroundColor = '#dcdcdc';
+        feedback.textContent = 'Try again';
+      }
+    }); //end of eventListener for squares
+  } //end of for loop
+} //end of function setUp
 
-//hard game mode for 6 colors
-hardBtn.addEventListener('click', function() {
-  //select hard button
-  hardBtn.classList.add('selected');
-
-  // remove selection from easy button
-  easyBtn.classList.remove('selected');
-
+//controls reseting the page
+function reset() {
   //generate and change colors on page
-  colors = generateColors(numSquares[0]);
-
-  //pick a new random color from array
-  pickedColor = randomColor();
-  color.textContent = pickedColor;
-
-  //show all squares with new colors
-  for (let i = 0; i < squares.length; i++) {
-    squares[i].style.backgroundColor = colors[i];
-    squares[i].style.display = 'block';
-  }
-});
-
-resetBtn.addEventListener('click', function() {
-  //generate and change colors on page
-  colors = generateColors(numSquares[0]);
+  colors = generateColors(numSquares);
 
   //pick a new random color form array
   pickedColor = randomColor();
-  color.textContent = pickedColor;
+
   for (let i = 0; i < squares.length; i++) {
-    //assign colors to squares
-    squares[i].style.backgroundColor = colors[i];
-  }
+    if (colors[i]) {
+      // show all squares
+      squares[i].style.display = 'block';
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      // hide 3 bottom squares for easy mode
+      squares[i].style.display = 'none';
+    }
+  } // end of for loop
+  color.textContent = pickedColor;
   topDiv.style.backgroundColor = 'steelblue';
-  this.textContent = 'New Colors';
+  resetBtn.textContent = 'New Colors';
   feedback.textContent = '';
+} // end of reset function
+
+//responsible for resetting the game
+//when new colors button is clicked
+resetBtn.addEventListener('click', function() {
+  reset();
 });
 
-color.textContent = pickedColor;
-
-for (let i = 0; i < squares.length; i++) {
-  //assign colors to squares
-  squares[i].style.backgroundColor = colors[i];
-
-  //add click listener
-  squares[i].addEventListener('click', function() {
-    //find color of clicked square
-    let clickedColor = this.style.backgroundColor;
-    //compare it to pickedColor
-    if (clickedColor === pickedColor) {
-      feedback.textContent = 'Correct!';
-      changeColors(clickedColor);
-      topDiv.style.backgroundColor = clickedColor;
-      resetBtn.textContent = 'Play again?';
-    } else {
-      this.style.backgroundColor = '#dcdcdc';
-      feedback.textContent = 'Try again';
-    }
-  });
-}
-
+//responsible for changing all square colors
+// to correct color
 function changeColors(color) {
   //loop through all squares and change colors
   //to match correct color
@@ -113,11 +98,15 @@ function changeColors(color) {
   }
 }
 
+//responsible for choosing random color
+// that should be guessed
 function randomColor() {
   let random = Math.floor(Math.random() * colors.length);
   return colors[random];
 }
 
+//responsible for generating array of
+//color choices
 function generateColors(num) {
   //make an array
   let arr = [];
@@ -128,6 +117,7 @@ function generateColors(num) {
   return arr;
 }
 
+//responsible for generating rgb color
 function randomColorPicker() {
   //pick red color from 0 to 255
   let red = Math.floor(Math.random() * 256);
